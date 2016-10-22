1a2f5a0eb37021daa569942588332926fb931b77
copperstick6 <copperstick6@gmail.com>
2016-10-21 19:11:24 -0500

    strategy for tomorrow

---
+++ APIlist.py
@@ -0,0 +1,18 @@
+#Below is the list of all CONFIRMED APIs we are using.
+#Twilio API: https://www.twilio.com/
+#Google Maps API: https://developers.google.com/maps/documentation/ Python.
+#Crises API: http://api.crisis.net/
+#ReliefWeb API: http://www.programmableweb.com/api/reliefweb
+
+#possible APIs:
+#Amazon Alexa API
+#IBM Watson???
+
+#Strategy for tomorrow:
+#1. get base functionality done:
+#Android guys get the app design down.
+#backend devs focus on those 4 APIs, additional if necessary
+#frontend figure out website
+#2. testing and implementation.
+#3. addition of extra functionalities
+#4. roll out completed versions.
